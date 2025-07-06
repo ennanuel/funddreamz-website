b92fd57dfaa56aefaa8f7f47cbbbd1a99be1b067
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-07-06 01:21:58 +0100

    Removed apostrophe

--- app/_components/home/About.tsx
+++ app/_components/home/About.tsx
@@ -9,7 +9,7 @@ export default function About() {
             <section className="mx-auto w-full max-w-lg">
                 <h3 className="text-6xl font-bold tracking-tighter"><span className="text-dark-green">Hi There! 🤗</span></h3>
                 <p className="mt-10 text-xl font-semibold tracking-tighter max-w-[64ch]">
-                    <span className="text-dark-green/60">Welcome to <span className="text-dark-green">FundDreamz</span>, your go-to platform for turning dreamz into reality. FundDreamz is a crowdfunding platform designed to help individuals, communities, startups, NGOs, and charities raise funds for their projects and initiatives. Whether you're an individual looking to get funding for a quick need/goal/dream, and enterpreneur with a groundbreaking idea, a community looking to make a positive change, or a chirty in need of support, FundDreamz provides you with the tools and resources to bring your dreams to life.</span>
+                    <span className="text-dark-green/60">Welcome to <span className="text-dark-green">FundDreamz</span>, your go-to platform for turning dreamz into reality. FundDreamz is a crowdfunding platform designed to help individuals, communities, startups, NGOs, and charities raise funds for their projects and initiatives. Whether you&pos;re an individual looking to get funding for a quick need/goal/dream, and enterpreneur with a groundbreaking idea, a community looking to make a positive change, or a chirty in need of support, FundDreamz provides you with the tools and resources to bring your dreams to life.</span>
                 </p>
                 <a href="#more" className="mt-4 w-fit flex items-center justify-center gap-2 hover:underline">
                     <FiArrowDown size={16} className="-mt-0.5" />
